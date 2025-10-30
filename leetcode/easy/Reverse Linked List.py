b83b94a96b22bee6232ebc65ae3ec21e5655934e
mohos26 <mhoussas26@gmail.com>
2025-10-30 22:35:51 +0100

    Reverse Linked List

---
+++ leetcode/easy/Reverse Linked List.py
@@ -0,0 +1,41 @@
+# https://leetcode.com/problems/reverse-linked-list
+# 30.10.2025
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        lst = []
+        while head:
+            lst.append(head.val)
+            head = head.next
+        lst.reverse()
+        if not lst:
+            return None
+        head = ListNode(lst[0])
+        tmp = head
+        for n in lst[1:]:
+            tmp2 = ListNode(n)
+            tmp.next = tmp2
+            tmp = tmp2
+        return head
+
+
+class Solution2:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        res = None
+        while head:
+            tmp = ListNode(head.val)
+            tmp.next = res
+            res = tmp
+            head = head.next
+        return res
+
+
+class Solution3:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        res = None
+        while head:
+            res = ListNode(head.val, res)
+            head = head.next
+        return res
+
