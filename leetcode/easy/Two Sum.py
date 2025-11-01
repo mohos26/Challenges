a8a1ca50db742831ad75698fdac2cb1131b6ed7d
mohos26 <mhoussas26@gmail.com>
2025-11-01 21:15:14 +0100

    Two Sum

---
+++ leetcode/easy/Two Sum.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/two-sum
+# 20.07.2025
+# 09.09.2025
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i, a in enumerate(nums):
+            for j, b in enumerate(nums[i+1:]):
+                if a + b == target:
+                    return [i, j + i + 1]
+        return []
+
+
+class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = Counter(nums)
+        for n in d:
+            if target - n in d.keys():
+                if 2 * n == target:
+                    if d[n] == 1:
+                        continue
+                i = nums.index(n)
+                j = nums[i+1:].index(target - n) + i + 1
+                return [i, j]
+        return []
+
