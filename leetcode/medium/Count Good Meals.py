7edeab2692f0238248b236b8e6c12e77309f5773
Mohamed Houssas <mhoussas26@gmail.com>
2025-11-29 15:49:20 +0100

    Count Good Meals

---
+++ leetcode/medium/Count Good Meals.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/count-good-meals
+# 29.11.2025
+
+
+class Solution:
+    def countPairs(self, deliciousness: List[int]) -> int:
+        result = 0
+        d = Counter(deliciousness)
+        for n in d:
+            for i in range(floor(log2(max(1, n))), 22):
+                if 2**i - n in d:
+                    if 2**i - n == n:
+                        if d[n] > 1:
+                            result += d[n] * (d[n] - 1) // 2
+                    else:
+                        result += d[n] * d[2**i-n]
+            d[n] = 0
+        return result % (10**9+7)
+
+
