04eff38adceca1bf6b11f06f6d435b6a32d92ae0
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-01 15:30:36 +0100

    Element Appearing More Than 25% In Sorted Array

---
+++ leetcode/easy/Element Appearing More Than 25% In Sorted Array.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/
+# 15.10.2025
+
+
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        d = Counter(arr)
+        length = len(arr)
+        for a, b in sorted(d.items(), reverse=True, key=lambda l:l[1]):
+            if b * 100 / length >= 25:
+                return a
+        return -1
+
+
+# 01.12.2025
+class Solution_2:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        return max(Counter(arr).items(), key=lambda l:l[1])[0]
+
