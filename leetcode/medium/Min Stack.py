48ae789c4f6de550a72ac4be2ddacd69f7c67c76
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-01 15:40:42 +0100

    Min Stack

---
+++ leetcode/medium/Min Stack.py
@@ -0,0 +1,44 @@
+# https://leetcode.com/problems/min-stack/
+# 19.10.2025
+
+
+class MinStack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return min(self.stack)
+
+
+# 20.10.2025
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.min:
+            self.min.append(min(val, self.min[-1]))
+        else:
+            self.min.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min[-1]
+
