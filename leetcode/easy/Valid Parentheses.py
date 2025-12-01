d4e3b22624a6f18178aae5e18b1acf6da75a2166
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-01 15:36:21 +0100

    Valid Parentheses

---
+++ leetcode/easy/Valid Parentheses.py
@@ -0,0 +1,41 @@
+# https://leetcode.com/problems/valid-parentheses
+# 23.07.2025
+
+
+class Solution:
+    d = {
+        '(': ')',
+        '{': '}',
+        '[': ']',
+    }
+
+    def isValid(self, s: str) -> bool:
+        lst = []
+        for arg in s:
+            if arg in "({[":
+                lst.append(arg)
+            elif lst and arg == self.d[lst[-1]]:
+                lst = lst[:-1]
+            else:
+                return False
+        return not lst
+
+
+# 19.10.2025
+class Solution:
+    d = {
+        '(': ')',
+        '{': '}',
+        '[': ']',
+    }
+    def isValid(self, s: str) -> bool:
+        lst = []
+        for arg in s:
+            if arg in self.d:
+                lst.append(arg)
+            elif lst and arg == self.d[lst[-1]]:
+                lst.pop()
+            else:
+                return False
+        return not lst
+
