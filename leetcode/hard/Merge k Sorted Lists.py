d32ec67e1d449b5900a00359a25972f2ed7e99e1
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-01 17:21:51 +0100

    Merge k Sorted Lists

---
+++ leetcode/hard/Merge k Sorted Lists.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/merge-k-sorted-lists/
+# 01.12.2025
+
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        dumpy = ListNode()
+        curr = dumpy
+        while True:
+            min_v = float("inf")
+            indices = []
+            for i in range(len(lists)):
+                if lists[i]:
+                    if min_v == lists[i].val:
+                        indices.append(i)
+                    elif min_v > lists[i].val:
+                        min_v = lists[i].val
+                        indices = [i]
+            if not indices:
+                break
+            for i in indices:
+                while lists[i] and lists[i].val == min_v:
+                    curr.next = ListNode(min_v)
+                    curr = curr.next
+                    lists[i] = lists[i].next
+        return dumpy.next
+
