dd374132616f9333d4c1608c4472990f8cab87f5
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-01 15:48:08 +0100

    Group Anagrams

---
+++ leetcode/medium/Group Anagrams.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/group-anagrams/
+# 09.09.2025
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = {}
+        for s in strs:
+            sorted_s = ''.join(sorted(s))
+            d[sorted_s] = d.get(sorted_s, []) + [s]
+        return list(d.values())
+
+
+# 20.10.2025
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = defaultdict(list)
+        for s in strs:
+            d[tuple(sorted(s))].append(s)
+        return list(d.values())
+
