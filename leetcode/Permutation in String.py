ade8a7d2c027b35fd4d20acdc1d1095e8363b1ea
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-01 15:32:06 +0100

    Permutation in String

---
+++ leetcode/Permutation in String.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/permutation-in-string/
+# 18.10.2025
+
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        k = len(s1)
+        d = Counter(s1)
+        aid = Counter(s2[:k])
+        if d == aid:
+            return True
+        for i in range(len(s2) - k):
+            aid[s2[i]] -= 1
+            aid[s2[i+k]] += 1
+            if d == aid:
+                return True
+        return False
+
