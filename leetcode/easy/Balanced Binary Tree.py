87fa769bd0d28ba69aac469eb860cf9b20e5201e
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-11 23:20:13 +0100

    Balanced Binary Tree (optimaze)

--- leetcode/easy/Balanced Binary Tree.py
+++ leetcode/easy/Balanced Binary Tree.py
@@ -2,7 +2,7 @@
 # 10.12.2025
 
 
-class Solution:
+class Solution: # Top-Down DFS o(n**2)
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
@@ -14,4 +14,18 @@ class Solution:
         if root and (not self.isBalanced(root.left) or not self.isBalanced(root.right)):
             return False
         return True
+
+class Solution_2: # Bottom-Up DFS o(n)
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node):
+            if not node:
+                return 0
+            left_depth = dfs(node.left)
+            if left_depth == -1:
+                return -1
+            right_depth = dfs(node.right)
+            if right_depth == -1 or abs(left_depth - right_depth) > 1:
+                return -1
+            return 1 + max(left_depth, right_depth)
+        return dfs(root) != -1
 
