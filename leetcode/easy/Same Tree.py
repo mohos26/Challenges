d8fc1b96f9adaf581a51aee1e517443ccce3f661
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-11 22:17:32 +0100

    Same Tree

---
+++ leetcode/easy/Same Tree.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/same-tree
+# 11.12.2025
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
+        return p == q or bool(p and q and p.val == q.val)
+
