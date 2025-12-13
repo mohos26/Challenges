f4654e22a070749bf2f9d1305735b397f27292a6
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-13 21:18:40 +0100

    Lowest Common Ancestor of a Binary Search Tree (optimaze)

--- leetcode/medium/Lowest Common Ancestor of a Binary Search Tree.py
+++ leetcode/medium/Lowest Common Ancestor of a Binary Search Tree.py
@@ -26,3 +26,16 @@ class Solution:
         dfs(root, p, q)
         return res
 
+
+class Solution_2:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        if root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        return left or right
+
