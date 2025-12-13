20ae0357927e70d49452d5ffe35de0dd18ba4efd
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-13 13:41:25 +0100

    Subtree of Another Tree

---
+++ leetcode/easy/Subtree of Another Tree.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/subtree-of-another-tree/
+# 13.12.2025
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
+        return p == q or bool(p and q and p.val == q.val)
+
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if self.isSameTree(root, subRoot):
+            return True
+        return root and bool(self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
+
