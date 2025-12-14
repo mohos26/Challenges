561bab36d39cd85a65122bcee28644fd81925765
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-14 18:32:14 +0100

    Binary Tree Level Order Traversal

---
+++ leetcode/medium/Binary Tree Level Order Traversal.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/binary-tree-level-order-traversal
+# 14.12.2025
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = [[] for _ in range(self.maxDepth(root))]
+        def dfs(node, depth):
+            if not node:
+                return
+            nonlocal res
+            dfs(node.left, depth+1)
+            dfs(node.right, depth+1)
+            res[depth].append(node.val)
+        dfs(root, 0)
+        return res
+
