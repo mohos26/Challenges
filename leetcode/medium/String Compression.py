b418f15edd2de553956ef0103d3aa3504976f12e
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-16 22:08:39 +0100

    String Compression

---
+++ leetcode/medium/String Compression.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/string-compression
+# 16.12.2025
+
+
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        i = 1
+        count = 1
+        while i < len(chars):
+            if count and chars[i] == chars[i-1]:
+                count += 1
+                chars.pop(i)
+            elif count > 1:
+                for n in str(count):
+                    chars.insert(i, n)
+                    i += 1
+                count = 0
+            else:
+                count = 1
+                i += 1
+        if count > 1:
+            chars.extend(list(str(count)))
+
