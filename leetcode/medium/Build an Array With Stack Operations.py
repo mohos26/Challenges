7299f30aac93d655ab87533874d5864cae829be9
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-19 13:16:44 +0100

    Build an Array With Stack Operations

---
+++ leetcode/medium/Build an Array With Stack Operations.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/build-an-array-with-stack-operations
+# 19.12.2025
+
+
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        i = 0
+        res = []
+        for n in range(1, n+1):
+            if i == len(target):
+                break
+            res.append('Push')
+            if n != target[i]:
+                res.append('Pop')
+            else:
+                i += 1
+        return res
+
