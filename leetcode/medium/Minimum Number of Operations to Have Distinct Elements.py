a9455618eb8514a9d7af4c767f79ae8432772d04
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-20 19:38:56 +0100

    Minimum Number of Operations to Have Distinct Elements

---
+++ leetcode/medium/Minimum Number of Operations to Have Distinct Elements.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/minimum-number-of-operations-to-have-distinct-elements
+# 20.12.2025
+
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        s = set(nums)
+        C = Counter(nums)
+        d = deque(nums)
+        lenght = len(nums)
+        res = 0
+        while True:
+            if len(s) == lenght:
+                break
+            for _ in range(3):
+                n = d.popleft()
+                C[n] -= 1
+                if C[n] == 0:
+                    s.remove(n)
+                lenght -= 1
+                if not d:
+                    break
+            res += 1
+        return res
+
+
+class Solution_2: # Cats7lives
+    def minOperations(self, nums: List[int]) -> int:
+        s = set()
+        for i in range(len(nums)-1, -1, -1):
+            if nums[i] in s:
+                return ceil((i+1) / 3)
+            s.add(nums[i])
+        return 0
+
