b884f8542da8877f188f2ca3da20b5083541c5b9
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-21 20:27:51 +0100

    Non-overlapping Intervals (optimize)

--- leetcode/medium/Non-overlapping Intervals.py
+++ leetcode/medium/Non-overlapping Intervals.py
@@ -17,3 +17,16 @@ class Solution:
             res += 1
         return length - res
 
+
+class Solution_2:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        res = 0
+        intervals.sort(key=lambda l: l[1])
+        end = intervals[0][1]
+        for i in range(1, len(intervals)):
+            if intervals[i][0] < end:
+                res += 1
+            else:
+                end = intervals[i][1]
+        return res
+
