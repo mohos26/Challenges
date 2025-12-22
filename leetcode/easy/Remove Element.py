d556bde9cf52f664963ca036ffbfe64d2c71fb71
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-22 22:09:54 +0100

    Remove Element

---
+++ leetcode/easy/Remove Element.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/remove-element
+# 22.12.2025
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.pop(i)
+            else:
+                i += 1
+
