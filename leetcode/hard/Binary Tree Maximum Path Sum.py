ec86873b338d6cef628b036fa3bd592c94db9ca9
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-23 17:24:11 +0100

    Binary Tree Maximum Path Sum

---
+++ leetcode/hard/Binary Tree Maximum Path Sum.py
@@ -0,0 +1,46 @@
+# https://leetcode.com/problems/binary-tree-maximum-path-sum
+# 23.12.2025
+
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = float("-inf")
+        def dfs(node):
+            if not node:
+                return None
+            nonlocal res
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            if left is None and right is None:
+                res = max(res, node.val)
+                return node.val
+
+            if left is None:
+                res = max(res, right + node.val, node.val)
+                return max(right + node.val, node.val)
+
+            if right is None:
+                res = max(res, left + node.val, node.val)
+                return max(left + node.val, node.val)
+
+            res = max(res, left + right + node.val, left + node.val, right + node.val, node.val)
+            return max(left + node.val, right + node.val, node.val)
+        dfs(root)
+        return res
+
+
+class Solution_2:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = float("-inf")
+        def dfs(node):
+            if not node:
+                return 0
+            nonlocal res
+            left = dfs(node.left)
+            right = dfs(node.right)
+            res = max(res, left + right + node.val)
+            return max(left + node.val, right + node.val, 0)
+        dfs(root)
+        return res
+
