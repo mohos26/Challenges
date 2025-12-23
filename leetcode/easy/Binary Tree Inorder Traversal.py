43d5efc5fc74a4cdcbae82c7ebad87505e7b634e
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-23 21:48:57 +0100

    Binary Tree Inorder Traversal

---
+++ leetcode/easy/Binary Tree Inorder Traversal.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/binary-tree-inorder-traversal
+# 23.12.2025
+
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        inorder = []
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)
+            inorder.append(node.val)
+            dfs(node.right)
+        dfs(root)
+        return inorder
+
