36c3ea4f27e32871f91ed11e3c2af15eabd6b3ec
mohos26 <mhoussas26@gmail.com>
2025-12-26 17:46:59 +0100

    Multiply Strings

---
+++ leetcode/medium/Multiply Strings.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/multiply-strings
+# 26.12.2025
+
+
+class Solution:
+    def ft_aid(self, a, b):
+        last = 0
+        res = ''
+        for i in b[::-1]:
+            res = str((a * int(i) + last) % 10) + res
+            last = (a * int(i) + last) // 10
+        res = str(last) + res if last else res
+        return res
+
+    def multiply(self, num1: str, num2: str) -> str:
+        res = 0
+        for i, n in enumerate(num2[::-1]):
+            res += int(self.ft_aid(int(n), num1))* 10**i
+        return str(res)
+
