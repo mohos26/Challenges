f35b1f3f13bcc0a0cef0abcd20dc1c14276ee18b
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-30 18:04:33 +0100

    Task Scheduler (optimaze)

--- leetcode/medium/Task Scheduler.py
+++ leetcode/medium/Task Scheduler.py
@@ -16,3 +16,44 @@ class Solution:
             res += n + 1 if d else len(lst)
         return res
 
+
+class Solution_2: # optimaze
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        res = 0
+        lst = sorted(Counter(tasks).values())
+        lst2 = deque()
+        counter = 0
+        while lst or lst2:
+            res += 1
+            counter += 1
+            if lst:
+                aid2 = lst.pop() - 1
+                if aid2:
+                    lst2.append(aid2)
+            if counter > n:
+                counter = 0
+                while lst2:
+                    bisect.insort(lst, lst2.pop())
+        return res
+
+
+class Solution_3: # heap
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        res = 0
+        counter = 0
+        lst = deque()
+        heap = [-value for value in Counter(tasks).values()]
+        heapq.heapify(heap)
+        while heap or lst:
+            res += 1
+            counter += 1
+            if heap:
+                lst.append(1 + heapq.heappop(heap))
+                if not lst[-1]:
+                    lst.pop()
+            if counter > n:
+                counter = 0
+                while lst:
+                    heapq.heappush(heap, lst.pop())
+        return res
+
