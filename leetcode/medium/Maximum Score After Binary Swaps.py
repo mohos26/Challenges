4aa7371292612ec131895608152e4835afe4ee1c
Mohamed Houssas <mhoussas26@gmail.com>
2025-12-31 10:45:08 +0100

    Maximum Score After Binary Swaps

---
+++ leetcode/medium/Maximum Score After Binary Swaps.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/maximum-score-after-binary-swaps
+# 31.12.2025
+
+
+class Solution:
+    def maximumScore(self, nums: List[int], s: str) -> int:
+        heap = []
+        length = 0
+        heap_len = 0
+        nums.reverse()
+        for a, b in zip(nums, s[::-1]):
+            length += int(b)
+            heapq.heappush(heap, a)
+            heap_len += 1
+            if heap_len > length:
+                heapq.heappop(heap)
+                heap_len -= 1
+        return sum(heap)
+
