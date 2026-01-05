7e479837288196984c6e73c9bbd9d40fc106a09d
Mohamed Houssas <mhoussas26@gmail.com>
2026-01-05 13:35:40 +0100

    Day of the Year

---
+++ leetcode/easy/Day of the Year.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/day-of-the-year/
+# 05.01.2025
+
+
+class Solution:
+    def dayOfYear(self, date: str) -> int:
+        dct = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+        y, m, d = map(int, date.split('-'))
+        if not y % 400 or (not y % 4 and y % 100):
+            dct[2] += 1
+        res = d
+        for i in range(1, m):
+            res += dct[i]
+        return res
