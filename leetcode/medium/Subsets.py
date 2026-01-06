47124b55cd949c96f621db46b074a2262f8ddf84
Mohamed Houssas <mhoussas26@gmail.com>
2026-01-06 18:07:04 +0100

    Subsets (recursion)

--- leetcode/medium/Subsets.py
+++ leetcode/medium/Subsets.py
@@ -14,3 +14,16 @@ class Solution:
             tmp.clear()
         return res
 
+
+class Solution_2: # recursion
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def dfs(lst, i):
+            if i == len(nums):
+                res.append(lst)
+                return
+            dfs(lst, i+1)
+            dfs(lst + [nums[i]], i+1)
+        dfs([], 0)
+        return res
+
