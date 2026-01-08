9d09ef65cc65a303255787de9f7538b65364f621
Mohamed Houssas <mhoussas26@gmail.com>
2026-01-08 21:41:19 +0100

    Subsets II

---
+++ leetcode/medium/Subsets II.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/subsets-ii
+# 08.01.2025
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        def dfs(lst, i):
+            if i == len(nums):
+                res.add(tuple(sorted(lst)))
+                return
+            lst.append(nums[i])
+            dfs(lst, i+1)
+            lst.pop()
+            dfs(lst, i+1)
+        dfs([], 0)
+        return list(res)
+
+
+class Solution_2:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        def dfs(lst, i):
+            if i == len(nums):
+                res.append(lst.copy())
+                return
+            lst.append(nums[i])
+            dfs(lst, i+1)
+            lst.pop()
+            for j in range(i+1, len(nums)):
+                if nums[i] != nums[j]:
+                    i = j - 1
+                    break
+            else:
+                i = len(nums) - 1
+            dfs(lst, i+1)
+        dfs([], 0)
+        return res
+
