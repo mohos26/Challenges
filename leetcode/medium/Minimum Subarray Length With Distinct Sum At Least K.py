0309ef7b17b24c4417c76232522e6400dc62c6f1
Mohamed Houssas <mhoussas26@gmail.com>
2026-01-08 21:09:32 +0100

    Minimum Subarray Length With Distinct Sum At Least K(optimaze)

--- leetcode/medium/Minimum Subarray Length With Distinct Sum At Least K.py
+++ leetcode/medium/Minimum Subarray Length With Distinct Sum At Least K.py
@@ -21,3 +21,28 @@ class Solution:
                 res = min(res, r - l + 1)
         return res if res != float("inf") else -1
 
+
+class Solution_2:
+    def minLength(self, nums: List[int], k: int) -> int:
+        l, r = 0, 0
+        _sum = 0
+        d = defaultdict(int)
+        res = float("inf")
+        for r in range(len(nums)):
+            if not d[nums[r]]:
+                _sum += nums[r]
+            d[nums[r]] += 1
+            if _sum >= k:
+                while l < r and _sum - nums[l] * (not d[nums[l]] - 1)>= k:
+                    d[nums[l]] -= 1
+                    if not d[nums[l]]:
+                        _sum -= nums[l]
+                    l += 1
+                res = min(res, r - l + 1)
+            if r - l > res:
+                d[nums[l]] -= 1
+                if not d[nums[l]]:
+                    _sum -= nums[l]
+                l += 1
+        return res if res != float("inf") else -1
+
