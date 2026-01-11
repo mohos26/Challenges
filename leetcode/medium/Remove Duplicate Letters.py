b7940c09993b924c4beabe19b77390875f488e51
Mohamed Houssas <mhoussas26@gmail.com>
2026-01-11 15:37:40 +0100

    Remove Duplicate Letters

---
+++ leetcode/medium/Remove Duplicate Letters.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/remove-duplicate-letters
+# 11.01.2025
+
+
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        stack = []
+        hash_table = set()
+        d = {s[i]:i for i in range(len(s))}
+        for i in range(len(s)):
+            letter = s[i]
+            if letter in hash_table:
+                continue
+            while stack and stack[-1] > letter and d[stack[-1]] > i:
+                hash_table.remove(stack.pop())
+            hash_table.add(letter)
+            stack.append(letter)
+        return ''.join(stack)
+
