5d6ea7a1ab2689f37e3bef1544d325c843c3b9f5
mohos26 <mhoussas26@gmail.com>
2026-01-18 08:46:44 +0100

    Smallest Subtree with all the Deepest Nodes

---
+++ leetcode/medium/Smallest Subtree with all the Deepest Nodes.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/
+# 18.01.2026
+
+
+class Solution:
+    def maxDeepest(self, root):
+        if not root:
+            return 0
+        return 1 + max(self.maxDeepest(root.left), self.maxDeepest(root.right))
+
+    def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        _max = self.maxDeepest(root)
+        res = None
+        def dfs(node, deep):
+            if not node:
+                return deep
+            deepest_left = dfs(node.left, deep + 1)
+            deepest_right = dfs(node.right, deep + 1)
+            if deepest_left == deepest_right and deepest_left == _max:
+                nonlocal res
+                res = node
+            return max(deepest_left, deepest_right)
+        dfs(root, 0)
+        return res
+
