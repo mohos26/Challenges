4f1687b92aaba064b243b421ec1625f82c1fd993
Mohamed Houssas <mhoussas26@gmail.com>
2026-01-26 22:34:35 +0100

    Number of Islands

--- leetcode/medium/Number of Islands.py
+++ leetcode/medium/Number of Islands.py
@@ -43,3 +43,39 @@ class Solution_2:
                     bfs(i, j)
         return res
 
+
+# 26.01.2026
+class DSU:
+    def __init__(self, length):
+        self.parent = list(range(length))
+
+    def find(self, n):
+        if self.parent[n] != n:
+            self.parent[n] = self.find(self.parent[n])
+        return self.parent[n]
+
+    def unoin(self, a, b):
+        self.parent[self.find(a)] = self.find(b)
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        M, N = len(grid), len(grid[0])
+        dsu = DSU(M * N)
+        for i in range(M):
+            for j in range(N):
+                idx = i * N + j
+                if grid[i][j] == "1":
+                    dsu.unoin(idx, idx)
+                    for a, b in ((i + 1, j), (i, j + 1)):
+                        if 0 <= a < M and 0 <= b < N and grid[a][b] == "1":
+                            idx2 = a * N + b
+                            dsu.unoin(idx, idx2)
+        hash_table = set()
+        for i in range(M):
+            for j in range(N):
+                idx = i * N + j
+                if grid[i][j] == "1":
+                    hash_table.add(dsu.find(idx))
+        return len(hash_table)
+
