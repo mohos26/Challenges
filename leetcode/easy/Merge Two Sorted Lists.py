016655e809b18d11acb1934f97f9c815e39c180a
Mohamed Houssas <mhoussas26@gmail.com>
2026-02-20 11:49:01 +0000

    Merge Two Sorted Lists (optimaze)

--- leetcode/easy/Merge Two Sorted Lists.py
+++ leetcode/easy/Merge Two Sorted Lists.py
@@ -31,3 +31,27 @@ class Solution:
                 list2 = list2.next
         return self.result
 
+
+# 20.02.2026
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dumpy = ListNode()
+        curr = dumpy
+        while list1 and list2:
+            if list1.val < list2.val:
+                curr.next = ListNode(list1.val)
+                list1 = list1.next
+            else:
+                curr.next = ListNode(list2.val)
+                list2 = list2.next
+            curr = curr.next
+        while list1:
+            curr.next = ListNode(list1.val)
+            list1 = list1.next
+            curr = curr.next
+        while list2:
+            curr.next = ListNode(list2.val)
+            list2 = list2.next
+            curr = curr.next
+        return dumpy.next
+
