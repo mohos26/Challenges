ad0ef5f20fd0129aad4d45552280044d0fea4c19
mohos26 <mhoussas26@gmail.com>
2026-03-17 15:26:45 +0000

    Coin Change (dp bottom-up)

--- leetcode/medium/Coin Change.py
+++ leetcode/medium/Coin Change.py
@@ -16,3 +16,14 @@ class Solution:
             return dp[n]        
         return -1 if dfs(0) == float("inf") else dp[0]
 
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+        for i in range(1, amount+1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], 1 + dp[i - coin])
+        return dp[amount] if dp[amount] != amount + 1 else -1
+
