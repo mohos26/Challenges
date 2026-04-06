d2ba67d0d0d691061cab95ae74151e1f2a26082d
Mohamed Houssas <mhoussas26@gmail.com>
2026-04-06 14:41:05 +0100

    Two Sum (optimaze)

--- leetcode/easy/Two Sum.py
+++ leetcode/easy/Two Sum.py
@@ -25,3 +25,13 @@ class Solution2:
                 return [i, j]
         return []
 
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, n in enumerate(nums):
+            d[n] = i
+        for i, n in enumerate(nums):
+            if target - n in d and i != d[target-n]:
+                return [i, d[target-n]]
+
