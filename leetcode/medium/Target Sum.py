a8535a446d76850d9eee8c35bac82b4c759b434d
Mohamed Houssas <mhoussas26@gmail.com>
2026-04-24 22:07:19 +0100

    Target Sum (optimaze)

--- leetcode/medium/Target Sum.py
+++ leetcode/medium/Target Sum.py
@@ -14,3 +14,36 @@ class Solution:
             return memo[key]
         return dfs(0, 0)
 
+
+# 24.04.2026
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        total_sum = sum(nums)
+
+        if abs(target) > total_sum:
+            return 0
+
+        offset = total_sum
+        dp = [[0] * (2 * total_sum + 1) for _ in range(len(nums) + 1)]
+        dp[len(nums)][target + offset] = 1
+        for i in range(len(nums)-1, -1, -1):
+            for j in range(-total_sum, total_sum + 1):
+                res = 0
+                if j + nums[i] <= total_sum:
+                    res += dp[i+1][nums[i] + j + offset]
+                if j - nums[i] >= -total_sum:
+                    res += dp[i+1][j - nums[i] + offset]
+                dp[i][j + offset] = res
+        return dp[0][offset]
+
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        dp = [defaultdict(int) for _ in range(len(nums) + 1)]
+        dp[0][0] = 1
+        for i in range(len(nums)):
+            for total, count in dp[i].items():
+                dp[i+1][total + nums[i]] += count
+                dp[i+1][total - nums[i]] += count
+        return dp[len(nums)][target]
+
