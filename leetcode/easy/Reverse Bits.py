a3844a2a20c342025525087e7674cce363f55baf
Mohamed Houssas <mhoussas26@gmail.com>
2026-06-18 10:16:56 +0100

    Reverse Bits

---
+++ leetcode/easy/Reverse Bits.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/reverse-bits/
+# 18.06.2026
+
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for _ in range(32):
+            res = (res << 1) + (n & 1)
+            n >>= 1
+        return res
+
