b002447b4aeebc429ba7f8657d375ac4409a8935
Mohamed Houssas <mhoussas26@gmail.com>
2026-06-18 10:07:56 +0100

    Counting Bits

---
+++ leetcode/easy/Counting Bits.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/counting-bits/
+# 18.06.2026
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = []
+        for i in range(n+1):
+            res.append(i.bit_count())
+        return res
+
