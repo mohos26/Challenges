7170e7af4ca41cb811dacc7188b773bf44ba0825
Mohamed Houssas <mhoussas26@gmail.com>
2026-07-07 15:49:05 +0100

    Multiply Strings

--- leetcode/medium/Multiply Strings.py
+++ leetcode/medium/Multiply Strings.py
@@ -1,5 +1,5 @@
 # https://leetcode.com/problems/multiply-strings
-# 26.12.2026
+# 26.12.2025
 
 
 class Solution:
@@ -17,4 +17,36 @@ class Solution:
         for i, n in enumerate(num2[::-1]):
             res += int(self.ft_aid(int(n), num1))* 10**i
         return str(res)
+
+
+# 07.07.2026
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        lst = []
+        num1, num2 = num1[::-1], num2[::-1]
+        for i, n1 in enumerate(num1):
+            rest = 0
+            curr = ""
+            for n2 in num2:
+                rest += int(n1) * int(n2)
+                curr = str(rest % 10) + curr
+                rest //= 10
+            if rest:
+                curr = str(rest) + curr
+            curr += '0' * i
+            lst.append(curr)
+        res = lst[0]
+        for num in lst[1:]:
+            tmp, res = res, ""
+            _max = max(map(len, (num, tmp)))
+            num, tmp = num.zfill(_max), tmp.zfill(_max)
+            rest = 0
+            for n1, n2 in zip(num[::-1], tmp[::-1]):
+                rest += int(n1) + int(n2)
+                res = str(rest % 10) + res
+                rest //= 10
+            if rest:
+                res = str(rest) + res
+        res = res.lstrip('0')
+        return res if res else '0'
 
